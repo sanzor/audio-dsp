4f8ad18ab6ff19183380b2a76b4588d24e43274c
Adrian Bercovici <adriansan_93@yahoo.com>
2025-06-28 15:47:03 +0300

    fixing the dsp controller

--- api/src/controllers/dsp_controller.rs
+++ api/src/controllers/dsp_controller.rs
@@ -1,4 +1,7 @@
 use actix_web::{post, web::{self, post}, HttpResponse};
+use actors::user_actor::user_actor::UserActor;
+use domain::{dsp_message::DspMessage, track::Track};
+use kameo::actor::ActorRef;
 use serde::Deserialize;
 use ulid::Ulid;
 
@@ -6,17 +9,32 @@ use crate::app_data::AppData;
 
 #[derive(Deserialize)]
 pub struct AddTrackParams{
-
+    user_id:String,
+    track:Track
 }
 #[post("/add-track")]
 async fn add_track(path:web::Json<AddTrackParams>,app_state:web::Data<AppData>)->HttpResponse{
     let request=path.into_inner();
     let guard=app_state.user_map.lock().await;
-    let id = Ulid::new().to_string();
-    let mut user_map=app_state.user_map.lock().await;
-    let rez=match user_map.insert(id, user_actor){
-        None => return HttpResponse::InternalServerError().body("Could not insert new user"),
-        Some(u)=>u
+    
+    let user=match get_user(&request.user_id, &app_state).await{
+        Some(u)=>u,
+        None=>return HttpResponse::NotFound().body("User not found")
     };
+
+    user.ask(DspMessage::Insert { user_name: , track_payload: () }).await
     HttpResponse::Created().json("User created")
+}
+
+async fn get_user(user_id:&str,app_state:&web::Data<AppData>)->Option<ActorRef<UserActor>>{
+    let user={
+        let guard=app_state.user_map.lock().await;
+        let u=match guard.get(user_id).cloned(){
+            Some(us)=>us,
+            None=>return None
+        };
+        u
+    };
+    Some(user)
+
 }
