3858460b2f3cba45fce31cc5dce75be708211cf6
Adrian Bercovici <adriansan_93@yahoo.com>
2025-10-16 10:48:27 +0300

    fix

--- api/src/dtos/claims.rs
+++ api/src/dtos/claims.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Claims {
     pub user_id: String,
     pub name: Option<String>,
