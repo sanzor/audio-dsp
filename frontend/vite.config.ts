59e9cbf84ef561e8c24c02bcc4a31bcf937b5fd5
sanzor <adriansan_93@yahoo.com>
2026-03-20 20:59:06 +0200

    c

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,16 +7,12 @@ export default defineConfig({
    server: {
     port: 3000, // 👈 your FE runs on http://localhost:3000
     host: true,
-    proxy: {
-      "/auth": "http://localhost:3080",
-      "/v1": "http://localhost:3080",
-    },
   },
   plugins: [react(),tailwindcss(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
-   
+
   },
 })
